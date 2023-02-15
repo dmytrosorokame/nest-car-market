385dfe61cbd71604df61c98f3e34194341a1bb71
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-15 18:54:19 +0200

    add interceptor globally

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,17 +11,14 @@ import {
   Post,
   Query,
   Session,
-  UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,15 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+  ],
 })
 export class UsersModule {}
