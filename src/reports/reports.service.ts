4f54c49b9fe5e66cdb6aaeebebd427fa99d819da
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-18 12:28:54 +0200

    add first method to reports service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  async create(dto: CreateReportDto) {
+    const report = await this.repo.create(dto);
+
+    return this.repo.save(report);
+  }
+}
