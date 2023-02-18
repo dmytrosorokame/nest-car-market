2e54d15ff79306811fd0f2627b7f2c7a6f220f57
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-18 12:25:11 +0200

    add valdiation for createReportDto & prepare controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { AuthGuard } from './../guards/auth.guard';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
   @Post()
-  createReport(@Body() dto: CreateReportDto) {}
+  @UseGuards(AuthGuard)
+  createReport(@Body() dto: CreateReportDto) {
+    return this.reportsService.create(dto);
+  }
 }
