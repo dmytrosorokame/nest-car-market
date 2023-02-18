c6a634e16159173f130ad71182c247a48666b7b0
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-18 13:32:23 +0200

    return only userId after creating report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,10 @@
 import { User } from './../users/user.entity';
 import { AuthGuard } from './../guards/auth.guard';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { CreateReportDto } from './dtos';
+import { CreateReportDto, ReportDto } from './dtos';
 import { ReportsService } from './reports.service';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +12,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() dto: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(dto, user);
   }
