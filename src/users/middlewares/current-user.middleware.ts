878be1a6765e24b7c31efdec1ab50d35f78d80c0
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-19 10:44:58 +0200

    add request global declare

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -12,7 +22,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     if (userId) {
       const user = await this.usersService.findOne(userId);
 
-      // @ts-ignore
       req.currentUser = user;
     }
 
